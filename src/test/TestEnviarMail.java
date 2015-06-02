3661219b0708be3e3d26ba8e89592997253250bb
Maxi <mcolombo87@gmail.com>
2015-06-02 03:54:04 -0300

    .

--- src/test/TestEnviarMail.java
+++ src/test/TestEnviarMail.java
@@ -1,10 +1,22 @@
 package test;
 
+import negocio.EnvioCorreo;
+
+
+
 public class TestEnviarMail {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		EnvioCorreo pruebaEnvio = new EnvioCorreo();
+		
+		try {
+			pruebaEnvio.EnviarCorreo(1, "Probando");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 
 	}
