1f14f7905c5568bf01c246ca80443558977aea8f
Maxi <mcolombo87@gmail.com>
2015-06-06 19:12:22 -0300

    Primera parte terminada, sin testear...

--- src/negocio/JornadaABM.java
+++ src/negocio/JornadaABM.java
@@ -69,11 +69,93 @@ public class JornadaABM
 			throw new Exception ("Error: La fecha ingresada es incorrecta");
 		}
 	}
+
+	public void generarJornadasMes()throws Exception 
+	{
+		//Alta automatica de jornadas
+		GregorianCalendar fecha = new GregorianCalendar();
+		String turno;
+		int grupo;
+		EmpleadoABM eABM = new EmpleadoABM();
+		List<Empleado> lstEmpleados = eABM.traerEmpleado();		
+		List<Empleado> lstGrupo = new ArrayList<Empleado>();
+
+		for(Empleado e : lstEmpleados)
+		{
+			//Primer empleado pivote
+			turno = e.getTurno().getTurno();
+			grupo = e.getGrupoTrabajo().getidGrupo();
+			lstGrupo.add(e);
+			lstEmpleados.remove(e);
+			for(Empleado e1 : lstEmpleados)
+			{
+				if (e1.getGrupoTrabajo().getidGrupo() == grupo)
+				{
+					lstGrupo.add(e1);
+					lstEmpleados.remove(e1);
+				}
+				
+			}
+			//Aca deberia tener un grupo completo para asignar jornadas
+			asignaJornada(lstGrupo, Funciones.traerMes(fecha), Funciones.traerAnio(fecha));
+		}
+					
+	}
 	
-	public void asignaJornada(List<Empleado> lstEmpleado, int mes, int anio){
-		for(Empleado e : lstEmpleado){
-			int checkFranco = 0;
-			for(int i=1; i<=Funciones.traerCantDiasDeUnMes(mes, anio); i++){
+	
+	public void asignaJornada(List<Empleado> lstEmpleado, int mes, int anio) throws Exception{
+		//Aprovecho el método pero debo hacerle modificaciones
+		int mesPasado = mes;
+		int anioPasado = anio;
+		int checkFranco = 0;
+		int diaComienzo = 1;
+		
+		//Evaluo si existe jornada mes anterior o es primera
+		if (mes == 1)//Prueba obligatoria para que no pinche en Enero
+		{
+			anioPasado = anio-1;
+		}else{mesPasado = mesPasado -1;}
+		
+		GregorianCalendar fechaMesPasado = new GregorianCalendar(anioPasado, mesPasado, Funciones.traerCantDiasDeUnMes(mesPasado, anioPasado));
+		if (evaluarMesAnterior(fechaMesPasado) == true)
+		{
+			//Aca tiene que hacer todo el lio para continuar el mes anterior del grupo
+			
+		}
+		else //Debe determinar donde comenzar (Si hay otro grupo asignado buscar Franco)
+		{
+			boolean listo = false;
+			while (listo == false)
+			{
+				GregorianCalendar fechaJornadaEvaluar = new GregorianCalendar(anio, mes, diaComienzo);
+				List<Jornada> lstjornadasEvaluar = traerJornadasPorFecha(fechaJornadaEvaluar);
+				if (lstjornadasEvaluar != null)
+				{
+					int z = 1;
+					boolean pasar = false;
+					while ( z <= lstjornadasEvaluar.size() && pasar == false)
+					{
+						if ( lstjornadasEvaluar.get(z).getTurno() == lstEmpleado.get(1).getTurno())
+						{
+							diaComienzo++;
+							pasar = true;
+						}
+					}
+					if (pasar == false)
+					{
+						listo =true;
+					}
+				}else{listo=true;}
+				if (diaComienzo >= Funciones.traerCantDiasDeUnMes(mes, anio))
+				{
+					listo=true;
+					diaComienzo=1;
+				}
+			}
+		}
+		for(Empleado e : lstEmpleado)
+		{
+			for(int i=diaComienzo; i<=Funciones.traerCantDiasDeUnMes(mes, anio); i++){
 				//asigno jornada
 				GregorianCalendar fechaJornada = new GregorianCalendar(anio, mes, i);
 				Jornada jornada = new Jornada(fechaJornada, e, e.getTurno());
@@ -85,6 +167,16 @@ public class JornadaABM
 				}
 			}
 		}
+	}
+	
+	private boolean evaluarMesAnterior(GregorianCalendar fechaMesAnterior) throws Exception
+	{
+		boolean resultado = false;
+		if ((traerJornadasPorFecha(fechaMesAnterior)) != null)
+		{
+			resultado = true;
+		}
+		return resultado;
 	}
 	
 	public void modificarJornada(Jornada j) throws Exception
